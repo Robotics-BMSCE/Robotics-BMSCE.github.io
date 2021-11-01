29535d30f6f0553cdb8ceac9e0ca51dec7f65cae
Raj Aryan <73771884+rajaryan18@users.noreply.github.com>
2021-11-01 11:13:52 +0530

    Running

--- src/home-page/Animated.js
+++ src/home-page/Animated.js
@@ -1,12 +1,22 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Canvas } from '@react-three/fiber';
 import './Animated.css';
 import Kick from './Kick';
-import { OrbitControls } from "@react-three/drei";
-import LoadingSpinner from '../helpers/LoadingSpinner';
+import { OrbitControls } from '@react-three/drei';
 
 const Animated = () => {
     return (
+<<<<<<< HEAD
+        <Canvas className="container">
+        
+            <OrbitControls enableZoom={false} minPolarAngle={Math.PI/2} maxPolarAngle={Math.PI/2} maxAzimuthAngle={Math.PI/15} minAzimuthAngle={-Math.PI/6} />  {/* to restrict the movements */}
+            <directionalLight intensity={0.5} />
+            <spotLight position={[2,2,0]} angle={Math.PI / 2} intensity={0.2} />  {/* The light direction can be adjusted by changing the position and angle */}
+            <Kick />  {/* Has the bot */}
+                
+        </Canvas>
+    
+=======
         <div className="container">
             <Canvas>
                 <Suspense fallback={<LoadingSpinner />}>   {/* Calls the file loading Spinner that displays a loading spinner */}
@@ -17,6 +27,7 @@ const Animated = () => {
                 </Suspense>
             </Canvas>
         </div>
+>>>>>>> a1abcc435bbea9b12bc81934cd2905383a12d690
     );
 };
 
