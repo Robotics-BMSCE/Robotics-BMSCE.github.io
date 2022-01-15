e6c808a0d8f145423590a0739ed7410086557e2d
Guru Nanma P <nanma.cs18@bmsce.ac.in>
2022-01-15 12:32:48 +0530

    🎆new webinar added

--- src/events/EventPage.js
+++ src/events/EventPage.js
@@ -6,11 +6,11 @@ import events from './events-data.json';
 const EventPage = () => {
     return (
         <div className="eventpage-container"> {/* Mapping all the upcoming events and past event */}
-            <h3 className="eventpage-h3">Upcoming Events</h3> {/* Upcoming -> type = true and Past -> type = false */}
-            {events.upcoming.map(e => { 
+            {/* <h3 className="eventpage-h3">Upcoming Events</h3> {/* Upcoming -> type = true and Past -> type = false */}
+            {/*{events.upcoming.map(e => { 
                 return <Event type={true} image={e.image} desc={e.desc} date={e.date} time={e.time} speaker={e.speaker} link={e.link} title={e.title} platform={e.platform} />
             })}
-            <hr /> 
+            <hr />  */}
             <h3 className="eventpage-h3">Past Events</h3>
             {events.past.map(e => {
                 return <Event type={false} image={e.image} desc={e.desc} date={e.date} time={e.time} speaker={e.speaker} link={e.link} title={e.title} platform={e.platform} />
